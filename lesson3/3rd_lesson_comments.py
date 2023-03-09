19d3d38aa763ec90518fe719f6495d0e7ee95119
пользователь <tsytokelectroob@yandex.ru>
2023-03-09 23:45:18 +0300

    Comments

---
+++ lesson3/3rd_lesson_comments.py
@@ -0,0 +1,32 @@
+#my_stng = 'Hello world!'
+#for i in range(len(my_stng)):
+ #   if my_stng[i] =='l':
+  #      print(my_stng[i])
+
+#name = input('Введите имя: ')
+#s_name = input('Введите фамилию: ')
+#flngs = input('Введите инфу о себе: ')
+#print('My name is {}. My second name is {}.Я {}'.format(name,s_name,flngs))
+#print(name.count('i')+s_name.count('i')+flngs.count('i'))
+
+#txt ='Ramsdale.White.Saliba.Gabriel.Zinchenko'
+#print(txt.split("."))
+
+#for i in txt.split("."):
+ #   print('Министр обороны: '+i)
+
+#list = [2,4,5,19,90]
+
+#for i in list:
+ #   if i%2==0:
+  #      print(i)
+
+
+'''Заводим список с n оличеством элементов и сами объекты через input'''
+#n = int(input('Введите количество элементов списка: '))
+#lst = []
+#for i in range(n):
+    #lst.append(input())
+
+#print(lst)
+'''Заводим список с n оличеством элементов и сами объекты через input'''
