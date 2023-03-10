de0906ddc843dbb8b82531d2df00cb77ff10c978
пользователь <tsytokelectroob@yandex.ru>
2023-03-10 15:20:15 +0300

    task1

---
+++ lesson3/3rd_lesson_comments+task1.py
@@ -0,0 +1,16 @@
+
+n =int(input('Введите порядковый номер месяца(от 1 до 12): '))
+
+months_dict ={1: 'january', 2: 'february', 3: 'march', 4: 'april', 5: 'may', 6: 'june', 7: 'july', 8: 'august', 9: 'september', 10: 'october', 11: 'november', 12: 'december'}
+
+for k, v in months_dict.items():
+    if k==n:
+        print(v)
+
+
+if n==2:
+    print('в этом месяце 28 день')
+elif n%2==0:
+    print('в этом месяце 30 дней')
+else:
+    print('в этом месяце 31 дней')
