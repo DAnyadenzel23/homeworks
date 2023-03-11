50c074e4e00b40f170fc255043b23c634d73f30e
пользователь <tsytokelectroob@yandex.ru>
2023-03-11 16:10:29 +0300

    lesson3 -homework(task1+task2)

--- lesson3/3rd_lesson_comments+task1.py
+++ lesson3/3rd_lesson_comments+task1.py
@@ -1,4 +1,4 @@
-
+'''1. Напишите программу, считывающую одно число N - номер месяца, и выводящую, сколько в  этом месяце дней и его название. (используйте словари)'''
 n =int(input('Введите порядковый номер месяца(от 1 до 12): '))
 
 months_dict ={1: 'january', 2: 'february', 3: 'march', 4: 'april', 5: 'may', 6: 'june', 7: 'july', 8: 'august', 9: 'september', 10: 'october', 11: 'november', 12: 'december'}
@@ -23,4 +23,15 @@ for i in range(q):
     list.append(input())
 
 print(list)
+
+'''2. Написать программу, считывающую список чисел и выводящую список в обратном порядке. Реализовать считывание чисел из одной строки через пробел.'''
+
+s =int(input('Введите количество элементов списка: '))
+
+list_numb =[]
+for i in range(s):
+    list_numb.append(input())
+
+list_numb.reverse()
+print(' '.join(list_numb))
 
