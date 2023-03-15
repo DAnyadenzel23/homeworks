76dd0b3938c554b9294e6ecd31f2f8de389dc842
пользователь <tsytokelectroob@yandex.ru>
2023-03-15 20:42:01 +0300

    1task

---
+++ HW4lesson/1st.py
@@ -0,0 +1,18 @@
+a = int(input('enter 1st numb: '))
+b = int(input('enter 2nd numb: '))
+str_name = input('enter operation: ')
+
+def func_1():
+    if str_name == '+':
+        print(a+b)
+    elif str_name == '-':
+        print(a-b)
+    elif str_name == '*':
+        print(a*b)
+    elif str_name == '/':
+        print(a/b)
+    else:
+        print('Чумачечий ввод')
+
+func_1()
+
