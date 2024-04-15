bc88867f485f10fe0f7a71de61b59df3545363d4
DAnyadenzel23 <126605004+DAnyadenzel23@users.noreply.github.com>
2024-04-15 23:18:22 +0300

    Delete 6lesson directory

--- 6lesson/1st taskHW.py
+++
@@ -1,7 +0,0 @@
-'''1. Напишите программу, принимающую на вход слова через запятую и выводящую слова в алфавитном порядке.
- Используйте List comprehension.'''
-
-random_order_words = input("Enter any words:").split(",")
-not_random_order_words = sorted([w.upper() for w in random_order_words])
-print(not_random_order_words)
-
--- 6lesson/4th taskHW.py
+++
@@ -1,7 +0,0 @@
-'''Используя random.sample сгенерируйте список случайных элементов. Используйте List comprehension.'''
-import random
-
-a = input('entar any elements: ').split( )
-print(a)
-b = [elem for elem in random.sample(a, 3)]
-print(b)
